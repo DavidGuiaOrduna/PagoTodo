b2a8e71fa9eed532b3938972a1fd3d2952d2c320
David Guia <david.guia@elektra.com.mx>
2023-06-09 22:43:26 -0600

    Initial Commit

---
+++ PagaTodo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PagaTodo
+//
+//  Created by David Fernando Guia Orduña on 09/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
